539ef3199ab0bad61895eeb66c4420a56cf72e5c
boazjames <boazjames2@yahoo.com>
2026-05-18 20:18:22 +0300

    Add Meta Pixel tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -85,6 +85,7 @@ export const metadata: Metadata = {
 };
 
 const GOOGLE_ANALYTICS_ID = "G-S7RXCGKKP4";
+const META_PIXEL_ID = "3696257577191635";
 const GLOBAL_BODY_CLASSES = [
   "wp-theme-lexend",
   "theme-lexend",
@@ -184,6 +185,25 @@ export default function RootLayout({
             gtag('config', '${GOOGLE_ANALYTICS_ID}');
           `}
         </Script>
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${META_PIXEL_ID}');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=${META_PIXEL_ID}&ev=PageView&noscript=1" />`,
+          }}
+        />
         <JsonLd data={buildOrganizationSchema()} />
         <JsonLd data={buildWebsiteSchema()} />
         <RouteChrome>{children}</RouteChrome>
